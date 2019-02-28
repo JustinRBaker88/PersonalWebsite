57b91c27cc28f758a76a34a01e417928b7cd1c97
Justin Baker <JustinRBaker88@gmail.com>
2019-02-28 14:20:19 -0800

    Additional code for initializing the db.

--- app/flaskr/db.py
+++ app/flaskr/db.py
@@ -4,6 +4,10 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
 def init_db():
     db = get_db()
 
